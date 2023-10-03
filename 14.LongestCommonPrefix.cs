c7c2abc435b74796b7b60ffa57003f26ea119e3a
Melih <58309701+Sirmelihy@users.noreply.github.com>
2023-10-03 04:42:14 +0300

    Create 14.LongestCommonPrefix.cs

---
+++ 14.LongestCommonPrefix.cs
@@ -0,0 +1,44 @@
+using System.Globalization;
+
+class Solution
+{
+
+
+    public static string LongestCommonPrefix(string[] strs) {
+        
+        string starter = strs[0];
+        string prefix = "";
+
+        for(int i = 1; i < strs.Length ; i ++ ) {
+            
+            for(int j = 0; j < strs[i].Length ; j ++) {
+
+                if( j < starter.Length && (starter[j] == strs[i][j] )) {
+                    prefix += strs[i][j];
+                }
+
+                else {
+                    
+                    break;
+                }
+
+                
+
+
+            }
+            starter = prefix;
+            prefix = "";
+        }
+
+        return starter;
+
+    }
+
+    static void Main(string[] args)
+    {
+
+        string[] asd = {"çakır" , "çakı" , "çak" , "çanta" };    //Test input
+
+        Console.WriteLine(LongestCommonPrefix(asd));             //Output should be "ça"
+    }
+}
